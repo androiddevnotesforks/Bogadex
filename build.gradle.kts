2bd3b00a8d42ba5e282f5a1774242114a597cc2a
Julien Foratier <jforatier@users.noreply.github.com>
2022-10-23 17:40:18 +0200

    :arrow_up: Bump gradle and dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,16 +16,16 @@ buildscript {
     }
     dependencies {
         // -- Core plugins
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // -- Application plugins
         classpath("com.google.dagger:hilt-android-gradle-plugin:$daggerVersion")
 
         // -- Services & Monitoring
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
-        classpath("com.google.firebase:perf-plugin:1.4.1")
+        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
+        classpath("com.google.firebase:perf-plugin:1.4.2")
     }
 }
 
